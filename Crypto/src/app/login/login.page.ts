843bb8afbe2d6cd40a886bd57af42605ebb4afb5
Rohan Sikder <80963667+rohansikder@users.noreply.github.com>
2023-12-01 13:55:21 +0000

    Added login button to home page and user is bought back to home if succesfull

--- Crypto/src/app/login/login.page.ts
+++ Crypto/src/app/login/login.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-login',
@@ -14,17 +18,30 @@ export class LoginPage implements OnInit {
     password: ''
   };
 
-  constructor(private authService: AuthenticationService) {}
+  constructor(private authService: AuthenticationService,private router: Router,private alertController: AlertController) {}
 
   // Method called when the user attempts to log in.
   login() {
     this.authService.login(this.user.email, this.user.password)
       .then(res => {
         console.log('Successfully logged in', res);
+        // Bring user back to home page
+        this.router.navigate(['/']);
       })
       .catch(err => {
         console.error('Login error', err);
+        this.showAlert("Login Failed", "Invalid email or password.");
       });
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+  
+    await alert.present();
   }
 
   ngOnInit() {
