19be8df6719d2786cb523a381f778f9ef58356ca
Rohan Sikder <80963667+rohansikder@users.noreply.github.com>
2023-12-03 07:42:21 +0000

    Created Sign up page

---
+++ Crypto/src/app/signup/signup.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.page.html',
+  styleUrls: ['./signup.page.scss'],
+})
+export class SignupPage {
+  user = { email: '', password: '' };
+
+  constructor() {}
+
+  signup() {
+
+  }
+}
