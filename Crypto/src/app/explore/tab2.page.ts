0d72916b8a75ab241bd0e8b635dc222ac3f7d32a
Rohan Sikder <80963667+rohansikder@users.noreply.github.com>
2023-12-26 22:23:43 +0000

    Added a dashboard where users overrall Financial data can be seen also allowing user to export to a csv file

--- Crypto/src/app/explore/tab2.page.ts
+++ Crypto/src/app/explore/tab2.page.ts
@@ -49,7 +49,7 @@ export class Tab2Page {
   }
 
   // Update trade with current price and handle notifications
-  async updateTradeWithCurrentPrice(trade: { symbol: string; buyPrice: number; stopLoss: number; takeProfit: number; quantity:number}) {
+  async updateTradeWithCurrentPrice(trade: { symbol: string; buyPrice: number; stopLoss: number; takeProfit: number; quantity: number }) {
     return this.cryptoService.getCryptoDetails(trade.symbol).then(data => {
       const currentPrice = parseFloat(data.data.priceUsd);
       console.log(trade.quantity);
@@ -158,5 +158,70 @@ export class Tab2Page {
     setTimeout(() => {
       loading.dismiss();
     }, 500);
+  }
+
+  // Dashboard Data Calculation Methods
+  get totalInvestment(): number {
+    return this.trades.reduce((acc, trade) => acc + (trade.buyPrice * trade.quantity), 0);
+  }
+
+  get currentValue(): number {
+    return this.trades.reduce((acc, trade) => acc + (trade.currentPrice * trade.quantity), 0);
+  }
+
+  get netProfitLoss(): number {
+    return this.currentValue - this.totalInvestment;
+  }
+
+  get averageBuyPrice(): number {
+    return this.trades.length > 0 ? this.totalInvestment / this.trades.length : 0;
+  }
+
+  get averageProfitLossPerTrade(): number {
+    return this.trades.length > 0 ? this.netProfitLoss / this.trades.length : 0;
+  }
+
+  get profitableTradesCount(): number {
+    return this.trades.filter(trade => trade.isProfit).length;
+  }
+
+  get losingTradesCount(): number {
+    return this.trades.filter(trade => !trade.isProfit).length;
+  }
+
+  // Method to convert trades data to CSV
+  // Method to convert trades data to CSV, including dashboard figures
+  exportTradesToCsv() {
+    // Dashboard summary
+    const dashboardSummary = `Total Investment,Current Value,Net Profit/Loss,Average Buy Price,Profitable Trades,Losing Trades\n` +
+      `${this.totalInvestment},${this.currentValue},${this.netProfitLoss},${this.averageBuyPrice},${this.profitableTradesCount},${this.losingTradesCount}\n\n`;
+
+    // Define the header for CSV file
+    const headers = 'Symbol,Buy Price,Quantity,Total Investment,Take Profit,Stop Loss,Current Price,Profit/Loss,ROI\n';
+
+    // Convert each trade to CSV format
+    const csvData = this.trades.map(trade => {
+      const profitLoss = (trade.currentPrice - trade.buyPrice) * trade.quantity;
+      const roi = ((trade.currentPrice - trade.buyPrice) / trade.buyPrice * 100).toFixed(2);
+      return `${trade.symbol},${trade.buyPrice},${trade.quantity},${trade.buyPrice * trade.quantity},${trade.takeProfit},${trade.stopLoss},${trade.currentPrice},${profitLoss.toFixed(2)},${roi}`;
+    }).join('\n');
+
+    // Combine dashboard summary, headers, and rows
+    const csvContent = dashboardSummary + headers + csvData;
+
+    // Trigger the download
+    this.downloadCsv(csvContent, 'trades.csv');
+  }
+
+  // Method to trigger the CSV download
+  private downloadCsv(csvContent: string, fileName: string) {
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.setAttribute('download', fileName);
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 }
