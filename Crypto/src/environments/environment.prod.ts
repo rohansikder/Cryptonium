129e35365b2226248066232b86b1e3f42970fa6e
Rohan Sikder <rohansikder4@gmail.com>
2024-01-05 15:23:12 +0000

    Built application through ionic and xcode for ios - Now can be deployed to iphone

--- Crypto/src/environments/environment.prod.ts
+++ Crypto/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDLuwERYu3mHubBO818OME5nWtJNqxWJKg",
+    authDomain: "cryptoproject-e49b2.firebaseapp.com",
+    projectId: "cryptoproject-e49b2",
+    storageBucket: "cryptoproject-e49b2.appspot.com",
+    messagingSenderId: "791382233811",
+    appId: "1:791382233811:web:68ccff2c678f285bc87a26"
+  }
 };
